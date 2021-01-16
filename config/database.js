9d633262e65c0be82c0b98543b94b3e907c3755f
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-01-16 10:52:07 -0800

    reconfig db

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  'mongodb://localhost:27017/' + process.env.DB_NAME,
+  process.env.DATABASE_URL,
   { useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
