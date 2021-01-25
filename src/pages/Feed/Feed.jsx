9a014d028cc63611e270b3005305c8ac3907cb5f
Aleksei Yarantsev <aleksei.yarantsev@gmail.com>
2021-01-25 14:31:15 -0800

    add logo at the front page, add semantic-ui styling

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -5,7 +5,7 @@ import React, { useState, useEffect } from 'react';
 import InputForm from "../../components/InputForm/InputForm";
 import PageHeader from '../../components/Header/Header';
 import StoryFeed from '../../components/StoryFeed/StoryFeed';
-import { Button, Input, Grid, Card, Icon } from 'semantic-ui-react';
+import { Button, Image, Grid, Card, } from 'semantic-ui-react';
 import AudioPlayer from 'react-h5-audio-player';
 import ReactPlayer from 'react-player'
 import 'react-h5-audio-player/lib/styles.css';
@@ -15,8 +15,10 @@ import base64 from 'react-native-base64';
 import fsReact from 'fs-react';
 import userService from '../../utils/userService';
 
+import logo from '../../utils/img/read4me-logo.png';
+
 export default function Feed({user, handleLogout}) {
- 
+  
   const [location, setLocation] = useState(); //location is the url path to S3 bucket with mp3 file
   const [playlist, setPlaylist] = useState();
   
@@ -37,26 +39,16 @@ export default function Feed({user, handleLogout}) {
 
   }
   
-async function testGetPlaylist(){
-
-  console.log('playlist: ', playlist);
-}
   return (
     <Grid centered >
     <Grid.Row>
       <Grid.Column>
         <PageHeader user={user}/>
-        <h1>This is the homepage!</h1>
+          <Image src={ logo } size='medium' centered />
       </Grid.Column>
     </Grid.Row>
-        <Grid.Row>
+        <Grid.Row centered>
             <Grid.Column style={{maxWidth: 450}}>
-
-              <AudioPlayer
-                autoPlay
-                src
-                ={location}
-                onPlay={e => console.log("onPlay")}/>
             </Grid.Column>
         </Grid.Row> 
         <Grid.Row>
@@ -65,22 +57,19 @@ async function testGetPlaylist(){
            <InputForm addToPlaylist={addToPlaylist}/>
           </Grid.Column>
         </Grid.Row>
-        <Grid.Row>
-          <Grid.Column>
-           
-            <button onClick={testGetPlaylist}>Test playlist</button>
+        <Grid.Row >
+          <Grid.Column>           
           {
           playlist && playlist.length ?
             playlist.map((story, index) => {
                 
                   return ( 
                     
-                    <Card key={index}>
+                    <Card fluid color='orange' key={index} centered >
                       <Card.Content header={story.title} />
                       <Card.Content description={story.description} />
                       <Card.Content extra>
                         <AudioPlayer
-
                           // autoPlay
                           src={story.location}
                           onPlay={e => console.log("onPlay")}/>
